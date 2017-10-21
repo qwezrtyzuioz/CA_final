f5abac22702c1c0da86a1e6013a4f350514f58d5
qwezrtyzuioz <qwezrtyzuioz@gmail.com>
2017-10-21 10:33:30 +0800

    Correct some bug

--- part_I/CNNConvLayer.cu
+++ part_I/CNNConvLayer.cu
@@ -152,7 +152,7 @@ void convLayerGPU(int* ifm, int* ifilt, int* outNeu, int* outGPU)
 	// Fill 128 corresponding filters.
 	// Using all 128 thread.
 	offset = filt_num * filt_vol + depth * filt_area;
-	for (int i = 0, i < filt_vol; ++i){
+	for (int i = 0, i < filt_area; ++i){
 		filt_index = offset + i;
 		filt[filt_index] = ifilt[depth * filt_vol + filt_index];
 	}
